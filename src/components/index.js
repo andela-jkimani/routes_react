87f91d7299268c8e4ecabf0c83684622b40264a4
andela-jkimani <jackline.kimani@andela.com>
2017-02-17 16:07:10 +0300

    better file structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './car/app'
+import React, { Component } from 'react'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import ReactDOM from 'react-dom';
+
+import Home from './common/Home.jsx';
+import About from './common/About.jsx';
+import Car from './car/Car.jsx';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Home} />
+        <Route path='/cars' component={Car} />
+        <Route path='/about' component={About} />
+      </Router>
+    )
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
