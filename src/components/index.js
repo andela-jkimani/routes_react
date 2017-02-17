043fede5c79517ff05d16e8df4e73b5f69a0f429
andela-jkimani <jackline.kimani@andela.com>
2017-02-17 21:19:08 +0300

    add routes

--- src/components/index.js
+++ src/components/index.js
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom';
 
 import Home from './common/Home.jsx';
 import About from './common/About.jsx';
+import Main from './common/Main.jsx';
 import Car from './car/Car.jsx';
 
 export default class App extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Home} />
-        <Route path='/cars' component={Car} />
-        <Route path='/about' component={About} />
+      <Router>
+        <Route component={Main} path='app'>
+          <IndexRoute component={Home} />
+          <Route path='/cars' component={Car} />
+          <Route path='/about' component={About} />
+        </Route>
       </Router>
     )
   }
